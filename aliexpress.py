c4065465b269835a2ae2b887d8c5e08537c0d2f1
Ruben <rubencolomer.1@gmail.com>
2019-02-19 12:45:13 +0100

    Get prices flash banggood

---
+++ aliexpress.py
@@ -0,0 +1,42 @@
+from urllib.request import urlopen
+import bs4
+import itertools
+
+class AliExpress(object):
+        """aliexpress Class"""
+        def __init__(self):
+            super(AliExpress, self).__init__()
+
+        def download_page(arg):
+            # connect to the web page
+            f = urlopen("https://www.banggood.com/es/Flashdeals.html")
+            # get the download_page
+            page = f.read()
+            # close the connection
+            f.close()
+            return page
+
+        def flash_offers(self, page):
+            tree = bs4.BeautifulSoup(page, "lxml")
+            ul = tree.find("ul", "goodlist_1")
+            li = ul.find_all("li")
+            title_list = []
+            for item in li:
+                price = item.find("span", "price").text
+                title = item.find("span", "title").text
+                title_list.append((title, price))
+            return title_list
+
+        def run(self):
+            # download a web page
+            page = self.download_page()
+            # search activities in web page
+            data = self.flash_offers(page)
+            # print the activities
+            print(data)
+
+
+
+if __name__ == "__main__":
+    c = AliExpress()
+    c.run()
