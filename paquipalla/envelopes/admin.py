2d2a332ce628c5c45de541ab09d969da464a7a70
Ruben <rubencolomer.1@gmail.com>
2019-02-26 12:46:01 +0100

    admin i envelopes

--- paquipalla/envelopes/admin.py
+++ paquipalla/envelopes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Enveolpe, Company
 # Register your models here.
+admin.site.register(Envelope)
+admin.site.register(Company)
--- paquipalla/envelopes/models.py
+++ paquipalla/envelopes/models.py
@@ -4,7 +4,13 @@ from django.db import models
 class Company(models.Model):
     name = model.Charfield(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Envelope(model.Model):
     amount = models.IntegerField()
     motivation = models-Charfield(max_length=100)
-    company = models.ForeignKey(Company)
+    company = models.ForeignKey(Company, on_delete=model.CASCADE)
+
+    def __str__(self):
+    return self.company.name+" * "+self.motive
